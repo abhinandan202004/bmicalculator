050b4e59213d38e3719e664b86290370aa09ed81
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-01-03 01:04:04 +0530

    Bootstrap deployed and adjustment done to javascript

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var weight = document.getElementById("weight").valueAsNumber
-var height = document.getElementById("height").valueAsNumber
-var bmi1 = bmi(weight,height);
+let w = document.getElementById("weight").valueAsNumber
+let h = document.getElementById("height").valueAsNumber
+let bmi1 = bmi(w,h);
+const button = document.getElementById("bt1")
+button.addEventListener('click', () => {
+    document.write(`Your BMI is ${bmi1}`,);
+
+});
+bmi1 = Math.round(bmi1*10) / 10;
 console.log(bmi1)
 output();
 function bmi(weight , height){
     var bmi = weight/height**2;
-    return bmi
+    return bmi;
 
 }
 
-function output(){
-    document.write("your BMI is " + bmi1)
-
-}
