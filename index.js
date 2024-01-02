33bcae0a965311be63a0ce1448367b1a71bb85cf
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-01-02 21:19:07 +0530

    basic structures and javascripts done (css required to be done)

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-weight = prompt("enter your weight")
-height = prompt("enter the height")
-bmi(weight,height);
+var weight = document.getElementById("weight").valueAsNumber
+var height = document.getElementById("height").valueAsNumber
+var bmi1 = bmi(weight,height);
+console.log(bmi1)
+output();
 function bmi(weight , height){
     var bmi = weight/height**2;
     return bmi
+
+}
+
+function output(){
+    document.write("your BMI is " + bmi1)
 
 }
