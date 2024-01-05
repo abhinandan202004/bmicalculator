cd21c9f0568d59adfaec84b5bf045887e743b4cc
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-01-06 00:56:42 +0530

    minor javascript error rectified

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let w = document.getElementById("weight").valueAsNumber
-let h = document.getElementById("height").valueAsNumber
-let bmi1 = bmi(w,h);
 const button = document.getElementById("bt1")
 button.addEventListener('click', () => {
-    
+    let w = document.getElementById("weight").valueAsNumber;
+    let h = document.getElementById("height").valueAsNumber;
+    let bmi1 = bmi(w,h);
+    bmi1 = Math.round(bmi1*10) / 10;
     document.write(`Your BMI is ${bmi1}`,);
 
 });
-bmi1 = Math.round(bmi1*10) / 10;
-console.log(bmi1)
+
+
 output();
-function bmi(weight , height){
-    var bmi = weight/height**2;
-    return bmi;
+function bmi(weight,height){
+    var bmi1 = weight/height**2;
+    return bmi1;
 
 }
 
