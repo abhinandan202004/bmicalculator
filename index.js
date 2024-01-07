7425acfbfc329ed787c2d882f9936e2f51864597
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-01-08 00:07:02 +0530

    Add BMI result evaluating function
    Introduce the result function to evaluate BMI conditions based on the calculated BMI value .
    Now the website can read the BMI of person and tell them their health condition

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ button.addEventListener('click', () => {
     let w = document.getElementById("weight").valueAsNumber;
     let h = document.getElementById("height").valueAsNumber;
     let bmi1 = bmi(w,h);
+    let condition = result(bmi1);
     bmi1 = Math.round(bmi1*10) / 10;
     document.getElementById("output").innerHTML = "Your BMI is " + bmi1 ;
+    document.getElementById("result").innerHTML = condition ;
 
 });
 
-
-output();
+ 
 function bmi(weight,height){
     var bmi1 = weight/height**2;
     return bmi1;
 
 }
-
+function result(bmi1) {
+    let condition;
+    if (bmi1 < 18.5)
+    {
+        condition = "underweight"
+    }
+    else if (bmi1 >= 18.5 || bmi1 < 25){
+        condition = "Normal"
+    }
+    else if (bmi1 >= 25 || bmi1 < 30 ){
+        condition = "overweight"
+    }
+    else if (bmi1 >= 30 || bmi1 < 35){
+        condition = "degree 1 obesity "
+    }
+    else if (bmi1 > 35){
+        condition = "degree 2 obesity"
+    }
+    return condition;
+}
+ 
